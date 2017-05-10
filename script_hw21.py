2cfc935aa28dc86bc293620a0199d5bc6e81b458
Дудаков Сергей <sdudakov@corp.local>
2017-05-10 18:04:51 +0300

    домашняя работа 21 лекция 5.3 исправления

--- script_hw21.py
+++ script_hw21.py
@@ -1,10 +1,13 @@
 from collections import deque
-linked_list = deque([1, 2, 3, 4, 5])
+linked_list = deque([1, 2, 3, 4, 5,])
+
 
 def reverse(linked_list):
+    temp_list = []
     linked_list_reversed = deque([])
     for element in linked_list:
-        linked_list_reversed.appendleft(element)
+        temp_list.append(element)
+    linked_list_reversed = temp_list[::-1]
     return linked_list_reversed
 
-print(linked_list, reverse(linked_list))
+print(reverse(linked_list))
